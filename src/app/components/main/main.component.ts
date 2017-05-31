6b77ba3544533ecadd4c3bdc4479e1847e3c075d
zm <maratn566ek@gmail.com>
2017-06-01 02:11:55 +0300

    resource

--- src/app/components/main/main.component.ts
+++ src/app/components/main/main.component.ts
@@ -7,10 +7,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MainComponent implements OnInit {
 	tiles = [
-		{text: 'One', cols: 3, rows: 1, color: 'lightblue'},
-		{text: 'Two', cols: 1, rows: 2, color: 'lightgreen'},
-		{text: 'Three', cols: 1, rows: 1, color: 'lightpink'},
-		{text: 'Four', cols: 2, rows: 1, color: '#DDBDF1'},
+		{text: 'One', cols: 3, rows: 1, color: 'lightblue',img:'jewerly1.png'},
+		{text: 'Two', cols: 1, rows: 2, color: 'lightgreen',img:'jewerly2.png'},
+		{text: 'Three', cols: 1, rows: 1, color: 'lightpink',img:'jewerly3.png'},
+		{text: 'Four', cols: 2, rows: 1, color: '#DDBDF1',img:'jewerly4.png'},
 	];
   constructor() { }
 
